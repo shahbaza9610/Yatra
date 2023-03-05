f919e20f0c8337f87aa1259c7536bdaf51a8b384
Farheen <shahbaza96@gmail.com>
2023-03-05 09:04:42 +0530

    New Class

---
+++ src/test/java/com/example/Yatra/BUS.java
@@ -0,0 +1,13 @@
+package com.example.Yatra;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class BUS {
+    @GetMapping("/mybus")
+    public String getData(){
+        return  "Please BOOK THE TICKET for BUS";
+    }
+}
